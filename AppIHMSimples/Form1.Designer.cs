75c2f72d47df6653c3b52e7e65ee04741e071486
Eduardo Henrique <eduardo.h.guerra@hotmail.com>
2020-05-23 15:59:29 -0300

    Inicio do projeto ComboBox Com Port

---
+++ AppIHMSimples/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace AppIHMSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.cbBoxPort = new System.Windows.Forms.ComboBox();
+            this.lblBaud = new System.Windows.Forms.Label();
+            this.cbBoxBaud = new System.Windows.Forms.ComboBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.cbBoxData = new System.Windows.Forms.ComboBox();
+            this.lblStop = new System.Windows.Forms.Label();
+            this.cbBoxStop = new System.Windows.Forms.ComboBox();
+            this.lblParity = new System.Windows.Forms.Label();
+            this.cbBoxParity = new System.Windows.Forms.ComboBox();
+            this.btnCom = new System.Windows.Forms.Button();
+            this.btnDes = new System.Windows.Forms.Button();
+            this.btnFec = new System.Windows.Forms.Button();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.pnlMsg = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pnl1.SuspendLayout();
+            this.pnlMsg.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(4, 4);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(73, 17);
+            this.lblPort.TabIndex = 0;
+            this.lblPort.Text = "COM Port:";
+            // 
+            // cbBoxPort
+            // 
+            this.cbBoxPort.FormattingEnabled = true;
+            this.cbBoxPort.Location = new System.Drawing.Point(83, 4);
+            this.cbBoxPort.Name = "cbBoxPort";
+            this.cbBoxPort.Size = new System.Drawing.Size(163, 24);
+            this.cbBoxPort.TabIndex = 1;
+            // 
+            // lblBaud
+            // 
+            this.lblBaud.AutoSize = true;
+            this.lblBaud.Location = new System.Drawing.Point(4, 50);
+            this.lblBaud.Name = "lblBaud";
+            this.lblBaud.Size = new System.Drawing.Size(79, 17);
+            this.lblBaud.TabIndex = 2;
+            this.lblBaud.Text = "Baud Rate:";
+            // 
+            // cbBoxBaud
+            // 
+            this.cbBoxBaud.FormattingEnabled = true;
+            this.cbBoxBaud.Location = new System.Drawing.Point(83, 50);
+            this.cbBoxBaud.Name = "cbBoxBaud";
+            this.cbBoxBaud.Size = new System.Drawing.Size(163, 24);
+            this.cbBoxBaud.TabIndex = 3;
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(4, 100);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(69, 17);
+            this.lblData.TabIndex = 4;
+            this.lblData.Text = "Data Bits:";
+            // 
+            // cbBoxData
+            // 
+            this.cbBoxData.FormattingEnabled = true;
+            this.cbBoxData.Location = new System.Drawing.Point(83, 100);
+            this.cbBoxData.Name = "cbBoxData";
+            this.cbBoxData.Size = new System.Drawing.Size(163, 24);
+            this.cbBoxData.TabIndex = 5;
+            // 
+            // lblStop
+            // 
+            this.lblStop.AutoSize = true;
+            this.lblStop.Location = new System.Drawing.Point(4, 150);
+            this.lblStop.Name = "lblStop";
+            this.lblStop.Size = new System.Drawing.Size(68, 17);
+            this.lblStop.TabIndex = 6;
+            this.lblStop.Text = "Stop Bits:";
+            // 
+            // cbBoxStop
+            // 
+            this.cbBoxStop.FormattingEnabled = true;
+            this.cbBoxStop.Location = new System.Drawing.Point(83, 150);
+            this.cbBoxStop.Name = "cbBoxStop";
+            this.cbBoxStop.Size = new System.Drawing.Size(163, 24);
+            this.cbBoxStop.TabIndex = 7;
+            // 
+            // lblParity
+            // 
+            this.lblParity.AutoSize = true;
+            this.lblParity.Location = new System.Drawing.Point(4, 200);
+            this.lblParity.Name = "lblParity";
+            this.lblParity.Size = new System.Drawing.Size(48, 17);
+            this.lblParity.TabIndex = 8;
+            this.lblParity.Text = "Parity:";
+            // 
+            // cbBoxParity
+            // 
+            this.cbBoxParity.FormattingEnabled = true;
+            this.cbBoxParity.Location = new System.Drawing.Point(83, 200);
+            this.cbBoxParity.Name = "cbBoxParity";
+            this.cbBoxParity.Size = new System.Drawing.Size(163, 24);
+            this.cbBoxParity.TabIndex = 9;
+            // 
+            // btnCom
+            // 
+            this.btnCom.Location = new System.Drawing.Point(7, 270);
+            this.btnCom.Name = "btnCom";
+            this.btnCom.Size = new System.Drawing.Size(117, 33);
+            this.btnCom.TabIndex = 10;
+            this.btnCom.Text = "Open Port";
+            this.btnCom.UseVisualStyleBackColor = true;
+            // 
+            // btnDes
+            // 
+            this.btnDes.Enabled = false;
+            this.btnDes.Location = new System.Drawing.Point(129, 270);
+            this.btnDes.Name = "btnDes";
+            this.btnDes.Size = new System.Drawing.Size(117, 33);
+            this.btnDes.TabIndex = 11;
+            this.btnDes.Text = "Close Port";
+            this.btnDes.UseVisualStyleBackColor = true;
+            // 
+            // btnFec
+            // 
+            this.btnFec.Location = new System.Drawing.Point(7, 334);
+            this.btnFec.Name = "btnFec";
+            this.btnFec.Size = new System.Drawing.Size(239, 33);
+            this.btnFec.TabIndex = 12;
+            this.btnFec.Text = "Exit";
+            this.btnFec.UseVisualStyleBackColor = true;
+            this.btnFec.Click += new System.EventHandler(this.btnFec_Click);
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnl1.Controls.Add(this.pnlMsg);
+            this.pnl1.Controls.Add(this.btnFec);
+            this.pnl1.Controls.Add(this.btnDes);
+            this.pnl1.Controls.Add(this.btnCom);
+            this.pnl1.Controls.Add(this.cbBoxParity);
+            this.pnl1.Controls.Add(this.lblParity);
+            this.pnl1.Controls.Add(this.cbBoxStop);
+            this.pnl1.Controls.Add(this.lblStop);
+            this.pnl1.Controls.Add(this.cbBoxData);
+            this.pnl1.Controls.Add(this.lblData);
+            this.pnl1.Controls.Add(this.cbBoxBaud);
+            this.pnl1.Controls.Add(this.lblBaud);
+            this.pnl1.Controls.Add(this.cbBoxPort);
+            this.pnl1.Controls.Add(this.lblPort);
+            this.pnl1.Location = new System.Drawing.Point(12, 12);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(250, 604);
+            this.pnl1.TabIndex = 0;
+            // 
+            // pnlMsg
+            // 
+            this.pnlMsg.BackColor = System.Drawing.Color.Red;
+            this.pnlMsg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlMsg.Controls.Add(this.label1);
+            this.pnlMsg.Location = new System.Drawing.Point(7, 403);
+            this.pnlMsg.Name = "pnlMsg";
+            this.pnlMsg.Size = new System.Drawing.Size(239, 191);
+            this.pnlMsg.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Open Port";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(268, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(602, 604);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(594, 575);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Monitor Serial";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(594, 575);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Botões/Leds";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(594, 575);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Sensores";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 631);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pnl1);
+            this.Name = "Form1";
+            this.Text = "IHM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl1.ResumeLayout(false);
+            this.pnl1.PerformLayout();
+            this.pnlMsg.ResumeLayout(false);
+            this.pnlMsg.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.ComboBox cbBoxPort;
+        private System.Windows.Forms.Label lblBaud;
+        private System.Windows.Forms.ComboBox cbBoxBaud;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.ComboBox cbBoxData;
+        private System.Windows.Forms.Label lblStop;
+        private System.Windows.Forms.ComboBox cbBoxStop;
+        private System.Windows.Forms.Label lblParity;
+        private System.Windows.Forms.ComboBox cbBoxParity;
+        private System.Windows.Forms.Button btnCom;
+        private System.Windows.Forms.Button btnDes;
+        private System.Windows.Forms.Button btnFec;
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Panel pnlMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ AppIHMSimples/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace AppIHMSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFec_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //Evento ocorre ao carregar o form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Vetor que recebe todos os nomes das portas
+            String[] valoresPort = SerialPort.GetPortNames();
+
+            //Adicionar os nomes das postar no comboBox COM Port
+            for (int i = 0; i < valoresPort.Length; i++)
+            {
+                cbBoxPort.Items.Add(valoresPort[i]);
+            }
+        }
+    }
+}
